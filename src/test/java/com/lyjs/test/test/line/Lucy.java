b97a640a29dcd6aadb324b7fb2423fff793015dd
sll0501 <2826258953@qq.com>
2022-09-16 10:59:29 +0800

    Alice相关操作

--- src/test/java/com/lyjs/test/test/line/Lucy.java
+++ src/test/java/com/lyjs/test/test/line/Lucy.java
@@ -2,6 +2,7 @@ package com.lyjs.test.test.line;
 
 public class Lucy {
     public static void main(String []args){
-        String str="Lucy修改相关代码";
+        String str="Alice修改相关代码";
+        String str2="Alice增加相关代码";
     }
 }
